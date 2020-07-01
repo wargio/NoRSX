c44ce6d7f0ec4e2e2b36c5f80f725287f22d71a8
Giovanni Dante Grazioli <giovanni.dantegrazioli@nbs-system.com>
2020-07-01 16:41:04 +0200

    refactoring

--- libNoRSX/Image.cpp
+++ libNoRSX/Image.cpp
@@ -22,448 +22,332 @@
 
 #include <NoRSX/Image.h>
 
-static u32 n, m, a, OxFF_A;
-static u32 *scr;
-static u32 *png;
-static u32 *jpg;
-static int NumPixels0 = 0;
-static int IntPart0 = 0;
-static int FractPart0 = 0;
-static int E0 = 0;
-static int NumPixels1 = 0;
-static int IntPart1 = 0;
-static int FractPart1 = 0;
-static int E1 = 0;
-static int height=0, width=0, stop_x = 0, stop_y = 0;
-
-static int error_x, error_y;
-
-void Image::LoadPNG(const char* filename, pngData *png){
+void Image::LoadPNG(const char* filename, pngData* png) {
 	pngLoadFromFile(filename, png);
 }
 
-void Image::LoadJPG(const char* filename, jpgData *jpg){
+void Image::LoadJPG(const char* filename, jpgData* jpg) {
 	jpgLoadFromFile(filename, jpg);
 }
 
-void Image::LoadPNG_Buf(const void* name, u32 name_size, pngData *png){
+void Image::LoadPNG_Buf(const void* name, u32 name_size, pngData* png) {
 	pngLoadFromBuffer(name, name_size, png);
 }
 
-void Image::LoadJPG_Buf(const void* name, u32 name_size, jpgData *jpg){
+void Image::LoadJPG_Buf(const void* name, u32 name_size, jpgData* jpg) {
 	jpgLoadFromBuffer(name, name_size, jpg);
 }
 
-void Image::DrawIMG(int x, int y, pngData *png1){
+inline void draw_image(s32 x, s32 y, u32* scr, u32 scr_width, u32 src_height, u32* img, u32 img_width, u32 img_height, u32 img_pitch) {
+	s32 error_x, error_y;
+	if (img) {
+		if (x < 0) {
+			img = img - x;
+			error_x = -x;
+			x = 0;
+		}
+		else {
+			error_x = 0;
+		}
 
-	if(png1->bmp_out){
-		u32 *scr = (u32 *)G->buffer;
-		u32 *png = (u32 *)png1->bmp_out;
+		if (y < 0) {
+			error_y = -y;
+			img = img + y * img_width;
+			y = 0;
+		}
+		else {
+			error_y = 0;
+		}
 
+		scr += y * scr_width + x;
+		s32 height = img_height;
+		if ((y + height) >= (s32)scr_height) {
+			height = scr_height - 1 - y;
+		}
 
-		if(x<0){
-			png = png-x;
-			error_x=-x;
-			x=0;
-		}else
-			error_x=0;
-			
-		if(y<0){
-			error_y=-y;
-			png = png + y*png1->width;
-			y=0;
-		}else
-			error_y=0;
-		
-		scr += y*G->width+x;
-		int height = png1->height;
-		if((y+height) >= (u32)G->height)
-			height = G->height-1-y;
+		s32 width = img_width;
+		if ((x + width) >= (s32)scr_width) {
+			width = scr_width - 1 - x;
+		}
 
-		int width = png1->width;
-		if((x+width) >= (u32)G->width)
-			width = G->width-x-1;
-
-		width-=error_x;
-		height-=error_y;
-		
-		if(width>0)
-		while(height>0){
-			memcpy(scr,png,width*sizeof(u32));
-			png+=png1->pitch>>2;
-			scr+=G->width;
+		width -= error_x;
+		height -= error_y;
+		while (width > 0 && height > 0) {
+			memcpy(scr, img, width * sizeof(u32));
+			img += img_pitch >> 2;
+			scr += scr_width;
 			height--;
 		}
 	}
 }
 
-void Image::DrawIMG(int x, int y, jpgData *jpg1){
-	if(jpg1->bmp_out){
-		u32 *scr = (u32 *)G->buffer;
-		u32 *jpg = (u32 *)(void *)jpg1->bmp_out;
-
-
-		if(x<0){
-			jpg = jpg-x;
-			error_x=-x;
-			x=0;
-		}else
-			error_x=0;
-		if(y<0){
-			error_y=-y;
-			jpg = jpg + y*jpg1->width;
-			y=0;
-		}else
-			error_y=0;
-
-		scr += y*G->width+x;
-		int height = jpg1->height;
-		if((y+height) >= (u32)G->height)
-			height = G->height-1-y;
-
-		int width = jpg1->width;
-		if((x+width) >= (u32)G->width)
-			width = G->width-1-x;
-
-		width-=error_x;
-		height-=error_y;
-		if(width>0)
-		while(height>0){
-			memcpy(scr,jpg,width*sizeof(u32));
-			jpg+=jpg1->pitch>>2;
-			scr+=G->width;
-			height--;
+inline void draw_image_alpha(s32 x, s32 y, u32* scr, u32 scr_width, u32 src_height, u32* img, u32 img_width, u32 img_height, u32 img_pitch) {
+	s32 error_x, error_y;
+	u32 m, a, OxFF_A;
+	if (img) {
+		if (x < 0) {
+			png = png - x;
+			error_x = -x;
+			x = 0;
+		} else {
+			error_x = 0;
 		}
-	}
-}
 
-void Image::AlphaDrawIMG(int x, int y, pngData *png1){
-	if(png1->bmp_out){
-		u32 *scr = (u32 *)G->buffer;
-		u32 *png = (u32 *)(void *)png1->bmp_out;
-		unsigned int m;
+		if (y < 0) {
+			error_y = -y;
+			png = png + y * img_width;
+			y = 0;
+		} else {
+			error_y = 0;
+		}
 
-		if(x<0){
-			png = png-x;
-			error_x=-x;
-			x=0;
-		}else
-			error_x=0;
-		if(y<0){
-			error_y=-y;
-			png = png + y*png1->width;
-			y=0;
-		}else
-			error_y=0;
+		scr += y * scr_width + x;
+		s32 height = img_height;
+		if ((y + height) >= (s32)scr_height) {
+			height = scr_height - 1 - y;
+		}
 
-		
-		scr += y*G->width+x;
-		int height = png1->height;
-		if((y+height) >= (u32)G->height)
-			height = G->height-1-y;
+		s32 width = img_width;
+		if ((x + width) >= (s32)scr_width) {
+			width = scr_width - 1 - x;
+		}
 
-		int width = png1->width;
-		if((x+width) >= (u32)G->width)
-			width = G->width-1-x;
+		width -= error_x;
+		height -= error_y;
 
-		width-=error_x;
-		height-=error_y;
-		
-		u32 a, OxFF_A;
-		
-		if(width>0)
-		while(height>0){
-			for(m=0;m < (u32)width;m++){
-				a = png[m] >> 24;	 // alpha 
-				if (a == 0xff)
+		while (width > 0 && height > 0) {
+			for (m = 0; m < (u32)width; m++) {
+				a = png[m] >> 24; // alpha
+				if (a == 0xff) {
 					scr[m] = png[m];
-				else if (a != 0){
+				} else if (a != 0) {
 					OxFF_A = 0xff - a;
-					scr[m] = (png[m] & 0xff000000) | ( (((((png[m] & 0x00ff00ff) * a) + ((scr[m] & 0x00ff00ff) *
-						 (OxFF_A))) & 0xff00ff00) | ((((png[m] & 0x0000ff00) * a) + ((scr[m] & 0x0000ff00) *
-						 (OxFF_A))) & 0x00ff0000)) >> 8);
+					scr[m] = (png[m] & 0xff000000) | 
+						((((((png[m] & 0x00ff00ff) * a) + ((scr[m] & 0x00ff00ff) * (OxFF_A))) & 0xff00ff00) |
+						((((png[m] & 0x0000ff00) * a) + ((scr[m] & 0x0000ff00) * (OxFF_A))) & 0x00ff0000)) >> 8);
 				}
 			}
-			png+=png1->pitch>>2;
-			scr+=G->width;
+			png += img_pitch >> 2;
+			scr += scr_width;
 			height--;
 		}
 	}
 }
 
-void Image::DrawPartialImage(int x, int y, unsigned int s_width, unsigned int s_height, unsigned int e_width, unsigned int e_height, unsigned int bg, unsigned int color, pngData *png1){
-	if(png1->bmp_out){
-		u32 *scr = (u32 *)G->buffer;
-		u32 *png = (u32 *)(void *)png1->bmp_out;
-		unsigned int n, m;
-
-		scr += y*G->width+x;
-		if(s_height>0) for(unsigned int i=0;i<s_height;i++) png+=png1->pitch>>2;
-		for(n=s_height;n<e_height+s_height;n++){
-			if((y+n)>=(u32)G->height) break;
-			for(m=s_width;m<e_width+s_width;m++){
-				if((x+m)>=(u32)G->width) break;
-				if(png[m]!=bg) scr[m-s_width]=color;
+inline void draw_image_partial(s32 x, s32 y, u32 s_width, u32 s_height, u32 e_width, u32 e_height, u32 bg, u32 color, u32* scr, u32 scr_width, u32* img, u32 img_pitch) {
+	u32 n, m;
+	if (img) {
+		scr += y * scr_width + x;
+		if (s_height > 0) {
+			for (u32 i = 0; i < s_height; i++) {
+				png += img_pitch >> 2;
 			}
-			png+=png1->pitch>>2;
-			scr+=G->width;
 		}
-	}
-}
-
-void Image::DrawPartialImage(int x, int y, unsigned int s_width, unsigned int s_height, unsigned int e_width, unsigned int e_height, unsigned int bg, unsigned int color, jpgData *jpg1){
-	if(jpg1->bmp_out){
-		u32 *scr = (u32 *)G->buffer;
-		u32 *jpg = (u32 *)(void *)jpg1->bmp_out;
-		unsigned int n, m;
-
-		scr += y*G->width+x;
-		if(s_height>0) for(unsigned int i=0;i<s_height;i++) jpg+=jpg1->pitch>>2;
-		for(n=s_height;n<e_height+s_height;n++){
-			if((y+n)>=(u32)G->height) break;
-			for(m=s_width;m<e_width+s_width;m++){
-				if((x+m)>=(u32)G->width) break;
-				if(jpg[m]!=bg) scr[m-s_width]=color;
+		for (n = s_height; n < e_height + s_height; n++) {
+			if ((y + n) >= (u32)scr_height) {
+				break;
 			}
-			jpg+=jpg1->pitch>>2;
-			scr+=G->width;
-		}
-	}
-}
-
-void Image::DrawIMGtoBitmap(int x, int y, pngData *png1, NoRSX_Bitmap *a){
-	if(png1->bmp_out){
-		u32 *scr = (u32 *)a->bitmap;
-		u32 *png = (u32 *)(void *)png1->bmp_out;
-
-		if(x<0){
-			png = png-x;
-			error_x=-x;
-			x=0;
-		}else
-			error_x=0;
-			
-		if(y<0){
-			error_y=-y;
-			png = png + y*png1->width;
-			y=0;
-		}else
-			error_y=0;
-		
-		scr += y*G->width+x;
-		int height = png1->height;
-		if((y+height) >= (u32)G->height)
-			height = G->height-1-y;
-
-		int width = png1->width;
-		if((x+width) >= (u32)G->width)
-			width = G->width-x-1;
-
-		width-=error_x;
-		height-=error_y;
-
-		if(width>0)
-		while(height>0){
-			memcpy(scr,png,width*sizeof(u32));
-			png+=png1->pitch>>2;
-			scr+=G->width;
-			height--;
-		}
-	}
-}
-
-
-void Image::DrawIMGtoBitmap(int x, int y, jpgData *jpg1, NoRSX_Bitmap *a){
-	if(jpg1->bmp_out){
-		u32 *scr = (u32 *)a->bitmap;
-		u32 *jpg = (u32 *)(void *)jpg1->bmp_out;
-
-		if(x<0){
-			jpg = jpg-x;
-			error_x=-x;
-			x=0;
-		}else
-			error_x=0;
-			
-		if(y<0){
-			error_y=-y;
-			jpg = jpg + y*jpg1->width;
-			y=0;
-		}else
-			error_y=0;
-		
-		scr += y*G->width+x;
-		int height = jpg1->height;
-		if((y+height) >= (u32)G->height)
-			height = G->height-1-y;
-
-		int width = jpg1->width;
-		if((x+width) >= (u32)G->width)
-			width = G->width-1-x;
-			
-			
-		width-=error_x;
-		height-=error_y;
-
-		if(width>0)
-		while(height>0){
-			memcpy(scr,jpg,width*sizeof(u32));
-			jpg+=jpg1->pitch>>2;
-			scr+=G->width;
-			height--;
-		}
-	}
-}
-
-
-void Image::AlphaDrawIMGtoBitmap(int x, int y, pngData *png1, NoRSX_Bitmap *a){
-	if(png1->bmp_out){
-		u32 *scr = (u32 *)a->bitmap;
-		u32 *png= (u32 *)(void *)png1->bmp_out;
-
-		if(x<0){
-			png = png-x;
-			error_x=-x;
-			x=0;
-		}else
-			error_x=0;
-			
-		if(y<0){
-			error_y=-y;
-			png = png + y*png1->width;
-			y=0;
-		}else
-			error_y=0;
-
-		scr += y*G->width+x;
-		int height = png1->height;
-		if((y+height) >= (u32)G->height)
-			height = G->height-1-y;
-
-		int width = png1->width;
-		if((x+width) >= (u32)G->width)
-			width = G->width-1-x;
-
-		width-=error_x;
-		height-=error_y;
-
-		u32 a, OxFF_A;
-
-		if(width>0)
-		for(n=0;n < (u32)height;n++){
-			for(m=0;m < (u32)width;m++){
-				a = png[m] >> 24;	 // alpha 
-				if (a == 0xff)
-					scr[m] = png[m];
-				else if (0 != a){
-					OxFF_A = 0xff - a;
-					scr[m] = (png[m] & 0xff000000)  | ((((((png[m] & 0x00ff00ff) * a) + ((scr[m] & 0x00ff00ff) *
-						 OxFF_A)) & 0xff00ff00) | ((((png[m] & 0x0000ff00) * a) + ((scr[m] & 0x0000ff00) *
-						 OxFF_A)) & 0x00ff0000)) >> 8);
+			for (m = s_width; m < e_width + s_width; m++) {
+				if ((x + m) >= (u32)scr_width) {
+					break;
+				}
+				if (png[m] != bg) {
+					scr[m - s_width] = color;
 				}
 			}
-			png+=png1->pitch>>2;
-			scr+=G->width;
+			png += img_pitch >> 2;
+			scr += scr_width;
 		}
 	}
 }
 
+void Image::DrawIMG(s32 x, s32 y, pngData* img) {
+	if (!img->bmp_out) {
+		return;
+	}
+	u32* scr        = (u32*)G->buffer;
+	u32  scr_width  = G->width;
+	u32  scr_height = G->height;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_width  = img->width;
+	u32  img_height = img->height;
+	u32  img_pitch  = img->pitch;
+	draw_image(x, y, scr, scr_width, scr_height, img, img_width, img_height, img_pitch);
+}
 
-void Image::ScaleLine(u32 *Target, u32 *Source, u32 SrcWidth, u32 TgtWidth){
- //Thanks to: http://www.compuphase.com/graphic/scale.htm
-	NumPixels1 = TgtWidth;
-	IntPart1 = SrcWidth / TgtWidth;
-	FractPart1 = SrcWidth % TgtWidth;
-	E1 = 0;
+void Image::DrawIMG(s32 x, s32 y, jpgData* img) {
+	if (!img->bmp_out) {
+		return;
+	}
+	u32* scr        = (u32*)G->buffer;
+	u32  scr_width  = G->width;
+	u32  scr_height = G->height;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_width  = img->width;
+	u32  img_height = img->height;
+	u32  img_pitch  = img->pitch;
+	draw_image(x, y, scr, scr_width, scr_height, img, img_width, img_height, img_pitch);
+}
 
-	while (NumPixels1-- > 0) {
-		*Target++ = *Source;
-		Source += IntPart1;
-		E1 += FractPart1;
-		if (E1 >= TgtWidth) {
-			E1 -= TgtWidth;
-			Source++;
-		} /* if */
-	} /* while */
+void Image::AlphaDrawIMG(s32 x, s32 y, pngData* img) {
+	if (!img->bmp_out) {
+		return;
+	}
+	u32* scr        = (u32*)G->buffer;
+	u32  scr_width  = G->width;
+	u32  scr_height = G->height;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_width  = img->width;
+	u32  img_height = img->height;
+	u32  img_pitch  = img->pitch;
+	draw_image_alpha(x, y, scr, scr_width, scr_height, img, img_width, img_height, img_pitch);
+}
+
+void Image::DrawPartialImage(s32 x, s32 y, u32 s_width, u32 s_height, u32 e_width, u32 e_height, u32 bg, u32 color, pngData* img) {
+	if (!img->bmp_out) {
+		return;
+	}
+
+	u32* scr        = (u32*)G->buffer;
+	u32  scr_width  = G->width;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_pitch  = img->pitch;
+	draw_image_partial(x, y, s_width, s_height, e_width, e_height, bg, color, scr, scr_width, u32* img, u32 img_pitch);
+}
+
+void Image::DrawPartialImage(s32 x, s32 y, u32 s_width, u32 s_height, u32 e_width, u32 e_height, u32 bg, u32 color, jpgData* img) {
+	if (!img->bmp_out) {
+		return;
+	}
+
+	u32* scr        = (u32*)G->buffer;
+	u32  scr_width  = G->width;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_pitch  = img->pitch;
+	draw_image_partial(x, y, s_width, s_height, e_width, e_height, bg, color, scr, scr_width, u32* img, u32 img_pitch);
+}
+
+void Image::DrawIMGtoBitmap(s32 x, s32 y, pngData* img, NoRSX_Bitmap* bm) {
+	if (!img->bmp_out) {
+		return;
+	}
+	u32* scr        = (u32*)bm->bitmap;
+	u32  scr_width  = bm->width;
+	u32  scr_height = bm->height;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_width  = img->width;
+	u32  img_height = img->height;
+	u32  img_pitch  = img->pitch;
+	draw_image(x, y, scr, scr_width, scr_height, img, img_width, img_height, img_pitch);
+}
+
+void Image::DrawIMGtoBitmap(s32 x, s32 y, jpgData* img, NoRSX_Bitmap* bm) {
+	if (!img->bmp_out) {
+		return;
+	}
+	u32* scr        = (u32*)bm->bitmap;
+	u32  scr_width  = bm->width;
+	u32  scr_height = bm->height;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_width  = img->width;
+	u32  img_height = img->height;
+	u32  img_pitch  = img->pitch;
+	draw_image(x, y, scr, scr_width, scr_height, img, img_width, img_height, img_pitch);
+}
+
+void Image::AlphaDrawIMGtoBitmap(s32 x, s32 y, pngData* img, NoRSX_Bitmap* a) {
+	if (!img->bmp_out) {
+		return;
+	}
+	u32* scr        = (u32*)bm->bitmap;
+	u32  scr_width  = bm->width;
+	u32  scr_height = bm->height;
+	u32* img        = (u32*)img->bmp_out;
+	u32  img_width  = img->width;
+	u32  img_height = img->height;
+	u32  img_pitch  = img->pitch;
+	draw_image_alpha(x, y, scr, scr_width, scr_height, img, img_width, img_height, img_pitch);
+}
+
+inline void scaleline(u32* target, u32* source, u32 src_width, u32 tgt_width) {
+	s32 num_pixels1 = 0;
+	s32 int_part1 = 0;
+	s32 fract_part1 = 0;
+	s32 e1 = 0;
+	//Thanks to: http://www.compuphase.com/graphic/scale.htm
+	num_pixels1 = tgt_width;
+	int_part1 = src_width / tgt_width;
+	fract_part1 = src_width % tgt_width;
+	e1 = 0;
+
+	while (num_pixels1-- > 0) {
+		*target++ = *source;
+		source += int_part1;
+		e1 += fract_part1;
+		if (e1 >= tgt_width) {
+			e1 -= tgt_width;
+			source++;
+		}
+	}
 	return;
 }
 
+inline resize_image(u32* source, u32* target, u32 in_width, u32 in_height, u32 tgt_width, u32 tgt_height) {
+	s32 e0 = 0;
+	s32 num_pixels0 = tgt_height;
+	s32 int_part0 = (in_height / tgt_height) * in_width;
+	s32 fract_part0 = in_height % tgt_height;
+	u32* prev_source = NULL;
 
-pngData * Image::ResizeImage(pngData *png_in, u32 TgtWidth, u32 TgtHeight){
- //Thanks to: http://www.compuphase.com/graphic/scale.htm
-	if(png_in->bmp_out){
-		pngData *png_out = new pngData;
-		png_out->bmp_out = (u32 *) malloc(TgtHeight*TgtWidth*sizeof(u32));
-		static u32 *Source = (u32 *)(void *)png_in->bmp_out;
-		static u32 *Target = (u32 *)(void *)png_out->bmp_out;
-
-		png_out->height = TgtHeight;
-		png_out->width  = TgtWidth;
-		png_out->pitch  = TgtWidth*4;
-
-		NumPixels0 = TgtHeight;
-		IntPart0 = (png_in->height / TgtHeight) * png_in->width;
-		FractPart0 = png_in->height % TgtHeight;
-		E0 = 0;
-		static u32 *PrevSource = NULL;
-
-		while (NumPixels0-- > 0) {
-			if (Source == PrevSource) {
-				memcpy(Target, Target-TgtWidth, TgtWidth*sizeof(*Target));
-			} else {
-				ScaleLine(Target, Source, png_in->width, TgtWidth);
-				PrevSource = Source;
-			}
-			Target += TgtWidth;
-			Source += IntPart0;
-			E0 += FractPart0;
-			if (E0 >= TgtHeight) {
-				E0 -= TgtHeight;
-				Source += png_in->width;
-			}
+	while (num_pixels0-- > 0) {
+		if (source == prev_source) {
+			memcpy(target, target - tgt_width, tgt_width * sizeof(u32));
+		} else {
+			scaleline(target, source, in_width, tgt_width);
+			prev_source = source;
 		}
+		target += tgt_width;
+		source += int_part0;
+		e0 += fract_part0;
+		if (e0 >= tgt_height) {
+			e0 -= tgt_height;
+			source += in_width;
+		}
+	}
+}
+
+pngData* Image::ResizeImage(pngData* png_in, u32 tgt_width, u32 tgt_height) {
+	//Thanks to: http://www.compuphase.com/graphic/scale.htm
+	if (png_in->bmp_out) {
+		pngData* png_out = new pngData;
+		png_out->bmp_out = (u32*)malloc(tgt_height * tgt_width * sizeof(u32));
+		u32* source = (u32*)png_in->bmp_out;
+		u32* target = (u32*)png_out->bmp_out;
+
+		png_out->height = tgt_height;
+		png_out->width = tgt_width;
+		png_out->pitch = tgt_width * 4;
+
+		resize_image(source, target, png_in->width, png_in->height, tgt_width, tgt_height);
 		return png_out;
 	}
 	return NULL;
 }
 
-jpgData * Image::ResizeImage(jpgData *jpg_in, u32 TgtWidth, u32 TgtHeight){
- //Thanks to: http://www.compuphase.com/graphic/scale.htm
-	if(jpg_in->bmp_out){
-		jpgData *jpg_out = new jpgData;
-		jpg_out->bmp_out = (u32 *) malloc(TgtHeight*TgtWidth*sizeof(u32));
-		static u32 *Source = (u32 *)(void *)jpg_in->bmp_out;
-		static u32 *Target = (u32 *)(void *)jpg_out->bmp_out;
+jpgData* Image::ResizeImage(jpgData* jpg_in, u32 tgt_width, u32 tgt_height) {
+	//Thanks to: http://www.compuphase.com/graphic/scale.htm
+	if (jpg_in->bmp_out) {
+		jpgData* jpg_out = new jpgData;
+		jpg_out->bmp_out = (u32*)malloc(tgt_height * tgt_width * sizeof(u32));
+		u32* source = (u32*)jpg_in->bmp_out;
+		u32* target = (u32*)jpg_out->bmp_out;
 
-		jpg_out->height = TgtHeight;
-		jpg_out->width  = TgtWidth;
-		jpg_out->pitch  = TgtWidth*4;
+		jpg_out->height = tgt_height;
+		jpg_out->width = tgt_width;
+		jpg_out->pitch = tgt_width * 4;
 
-		NumPixels0 = TgtHeight;
-		IntPart0 = (jpg_in->height / TgtHeight) * jpg_in->width;
-		FractPart0 = jpg_in->height % TgtHeight;
-		E0 = 0;
-		static u32 *PrevSource = NULL;
-
-		while (NumPixels0-- > 0) {
-			if (Source == PrevSource) {
-				memcpy(Target, Target-TgtWidth, TgtWidth*sizeof(*Target));
-			} else {
-				ScaleLine(Target, Source, jpg_in->width, TgtWidth);
-				PrevSource = Source;
-			}
-			Target += TgtWidth;
-			Source += IntPart0;
-			E0 += FractPart0;
-			if (E0 >= TgtHeight) {
-				E0 -= TgtHeight;
-				Source += jpg_in->width;
-			}
-		}
+		resize_image(source, target, jpg_in->width, jpg_in->height, tgt_width, tgt_height);
 		return jpg_out;
 	}
 	return NULL;
 }
-
-
-
