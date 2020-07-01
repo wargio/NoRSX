5b5d9c289addfcd7d50d1086a72267fe03e6f288
Giovanni Dante Grazioli <giovanni.dantegrazioli@nbs-system.com>
2020-07-01 17:45:36 +0200

    Refactoring example

--- NoRSX_Example/source/main.cpp
+++ NoRSX_Example/source/main.cpp
@@ -23,43 +23,58 @@
 #include "Sans_ttf.h"
 #include <time.h>
 
-msgType MSG_OK = (msgType)(MSG_DIALOG_NORMAL | MSG_DIALOG_BTN_TYPE_OK | MSG_DIALOG_DISABLE_CANCEL_ON);
+const msgType MSG_OK = (msgType)(MSG_DIALOG_NORMAL | MSG_DIALOG_BTN_TYPE_OK | MSG_DIALOG_DISABLE_CANCEL_ON);
 
-s32 main(s32 argc, const char* argv[]) {
+typedef struct _timer {
+	time_t start;
+	u32    frame;
+} timer_t;
+
+typedef struct _joypads {
 	padInfo padinfo;
 	padData paddata;
-	ioPadInit(7);
+} joypads_t;
 
-	int Bx = 0;
-	int By = 0;
+double calculate_fps(timer_t *timer) {
+	timer->frame++;
+	if (timer->start != 0) {
+		return timer->frame / difftime(time(NULL), timer->start);
+	}
+	timer->start = time(NULL);
+	return 1;
+}
 
-	pngData* png = new pngData;
+int handle_pad_event(joypads_t *joypads) {
+	ioPadGetInfo(joypads->padinfo);
+	if (joypads->padinfo.status[0]) {
+		ioPadGetData(0, joypads->paddata);
+		if (joypads->paddata.BTN_TRIANGLE) {
+			return 1;
+		}
+		if (joypads->paddata.BTN_START) {
+			return 2;
+		}
+	}
+	return 0;
+}
 
-	NoRSX* GFX = new NoRSX(RESOLUTION_AUTO, RESOLUTION_1280x720); //set defined screen resolution You can change it to:
-	//RESOLUTION_720x480 | RESOLUTION_720x576 | RESOLUTION_1280x720 | RESOLUTION_1920x1080
-	Image IMG(GFX);
-	Background BG(GFX);
-	Object OBJ(GFX);
-	Bitmap BMap(GFX);
-	MsgDialog Msg(GFX);
-
-	NoRSX_Bitmap Precalculated_Layer;
-
-	BMap.GenerateBitmap(&Precalculated_Layer); //Initialize the Bitmap
+int test_bitmap(NoRSX *GFX) {
+	joypads_t joypads = {0};
+	timer_t timer = {0};
 
 	Font F1(Sans_ttf, Sans_ttf_size, GFX); //Loaded from Memory
-	Font F2("/dev_flash/data/font/SCE-PS3-VR-R-LATIN2.TTF", GFX); //Loaded from File!
-	Font F3(JPN, GFX); //I'm Using PS3 Original Fonts! These are the available on the ps3: LATIN2 | JPN | KOR | CGB | KANA
+	Bitmap BMap(GFX);
+	Background BG(GFX);
+	Image IMG(GFX);
+	Object OBJ(GFX);
 
-	IMG.LoadPNG_Buf(NoRSX_Image_png, NoRSX_Image_png_size, png);
+	NoRSX_Bitmap Precalculated_Layer;
+	pngData* png = new pngData;
 
-	png = IMG.ResizeImage(png, 500, 500); //pngData* IMG.ResizeImage(pngData*, new width, new height)
-
-	u32 imgX = (GFX->width / 2) - (png->width / 2), imgY = (GFX->height / 2) - (png->height / 2);
-
+	BMap.GenerateBitmap(&Precalculated_Layer); //Initialize the Bitmap
 	BG.MonoBitmap(0xb4e83a, &Precalculated_Layer); //a green hex color (you can use hex colors insted of COLOR_XXXXXXX)
 
-	//	IMG.DrawIMGtoBitmap(imgX,imgY,png,&Precalculated_Layer);
+	IMG.DrawIMGtoBitmap(imgX, imgY, png, &Precalculated_Layer);
 
 	OBJ.CircleToBitmap(500, 500, 50, COLOR_YELLOW, &Precalculated_Layer);
 
@@ -68,93 +83,103 @@ s32 main(s32 argc, const char* argv[]) {
 	F2.PrintfToBitmap(150, 300, &Precalculated_Layer, COLOR_GREEN, 60, "FreeType2 with TTF support :)");
 	F3.PrintfToBitmap(150, 500, &Precalculated_Layer, "Written by deroad");
 
-	int frame = 0;
-	// Bitmap
+	// Loading image from memory
+	IMG.LoadPNG_Buf(NoRSX_Image_png, NoRSX_Image_png_size, png);
+
+	newpng = IMG.ResizeImage(png, 500, 500); //pngData* IMG.ResizeImage(pngData*, new width, new height)
+
+	u32 imgX = (GFX->width  / 2) - (newpng->width  / 2);
+	u32 imgY = (GFX->height / 2) - (newpng->height / 2);
 
 	GFX->AppStart();
 	while (GFX->GetAppStatus()) {
-		static time_t starttime = 0;
-		double fps = 0;
-		if (starttime == 0)
-			starttime = time(NULL);
-		else
-			fps = frame / difftime(time(NULL), starttime);
-		ioPadGetInfo(&padinfo);
-		if (padinfo.status[0]) {
-			ioPadGetData(0, &paddata);
-			if (paddata.BTN_TRIANGLE) {
-				GFX->AppExit();
-				break;
-			}
-			if (paddata.BTN_START) {
-				GFX->AppExit();
-				goto end;
-			}
+
+		double fps = calculate_fps(&timer);
+
+		int event;
+		if ((event = handle_pad_event(&joypads))) {
+			GFX->AppExit();
+			return event;
 		}
+
 		BMap.DrawBitmap(&Precalculated_Layer);
-		IMG.DrawIMG(imgX, imgY, png);
+		IMG.DrawIMG(imgX, imgY, newpng);
 		F1.Printf(150, 100, COLOR_RED, 60, "FPS %f", fps);
 
 		GFX->Flip();
-		frame++;
 	}
+
+	//You need to clean the Bitmap before exit
+	BMap.ClearBitmap(&Precalculated_Layer);
+
+	delete newpng;
+	delete png;
+
+	return 0;
+}
+
+int test_generic(NoRSX *GFX) {
+	joypads_t joypads = {0};
+	timer_t timer = {0};
+
+	Font F1(Sans_ttf, Sans_ttf_size, GFX); //Loaded from Memory
+	Font F2("/dev_flash/data/font/SCE-PS3-VR-R-LATIN2.TTF", GFX); //Loaded from File!
+	Font F3(JPN, GFX); //I'm Using PS3 Original Fonts! These are the available on the ps3: LATIN2 | JPN | KOR | CGB | KANA
+	Image IMG(GFX);
+	Background BG(GFX);
+
+	pngData* png = new pngData;
+	// Loading image from memory
+	IMG.LoadPNG_Buf(NoRSX_Image_png, NoRSX_Image_png_size, png);
 
 	GFX->AppStart();
 	while (GFX->GetAppStatus()) {
-		static time_t starttime = 0;
-		double fps = 0;
-		if (starttime == 0)
-			starttime = time(NULL);
-		else
-			fps = frame / difftime(time(NULL), starttime);
-		ioPadGetInfo(&padinfo);
-		if (padinfo.status[0]) {
-			ioPadGetData(0, &paddata);
-			if (paddata.BTN_TRIANGLE) {
-				GFX->AppExit();
-				break;
-			}
-			if (paddata.BTN_START) {
-				GFX->AppExit();
-				goto end;
-			}
+
+		double fps = calculate_fps(&timer);
+
+		int event;
+		if ((event = handle_pad_event(&joypads))) {
+			GFX->AppExit();
+			return event;
 		}
+
 		BG.Mono(0xb4e83a);
 		F1.Printf(150, 100, COLOR_RED, 60, "FPS %f", fps);
 		F1.Printf(150, 200, COLOR_RED, "Screen %d x %d", GFX->width, GFX->height);
 		F1.Printf(150, 250, COLOR_BLUE, 35, "Press /\\ to exit! (Start to skip Message Dialogs and exit)");
 		F2.Printf(150, 300, COLOR_GREEN, 60, "FreeType2 with TTF support :)");
 		F3.Printf(150, 500, "Written by deroad");
-		IMG.DrawIMG(imgX, imgY, png);
+		IMG.DrawIMG(1000, 250, png);
 		GFX->RescaleFlip();
-		frame++;
-	}
-	if (GFX->ExitSignalStatus()) {
-		goto end;
 	}
 
-	//You need to clean the Bitmap before exit
-	BMap.ClearBitmap(&Precalculated_Layer);
+	delete png;
+
+	return 0;
+}
+
+int test_single_bar(NoRSX *GFX) {
+	joypads_t joypads = {0};
+	int frame = 0;
+
+	MsgDialog Msg(GFX);
 
 	Msg.TimerErrorDialog(0xdeadbeef, 5000.f);
-
 	Msg.TimerDialog(MSG_OK, "Timer Dialog!", 5000.f);
 	Msg.TimerErrorDialog(0xdeadbeef, 5000.f);
 
+	int Bx = 0;
 	GFX->AppStart();
-	frame = 0;
+
 	Msg.SingleProgressBarDialog("Single progress bar!! (start to exit)", "Deroad Bar text");
 	while (GFX->GetAppStatus() && Bx < 100) {
-		ioPadGetInfo(&padinfo);
-		if (padinfo.status[0]) {
-			ioPadGetData(0, &paddata);
-			if (paddata.BTN_START) {
-				GFX->AppExit();
-				goto end;
-			}
-		}
-		Msg.ProgressBarDialogFlip();
 		frame++;
+		if (handle_pad_event(&joypads) == 2) {
+			GFX->AppExit();
+			return 2;
+		}
+
+		Msg.ProgressBarDialogFlip();
 		if (frame % 55 == 0) {
 			Msg.SingleProgressBarDialogIncrease(10);
 			Bx += 10;
@@ -162,21 +187,28 @@ s32 main(s32 argc, const char* argv[]) {
 	}
 	Msg.ProgressBarDialogAbort();
 
-	GFX->AppStart();
+	return 0;
+}
+
+int test_double_bar(NoRSX *GFX) {
+	joypads_t joypads = {0};
+	int frame = 0;
+
+	MsgDialog Msg(GFX);
+
 	Msg.DoubleProgressBarDialog("Double progress bar!! (start to exit)", "Deroad Bar1 text", "Deroad Bar2 text");
-	Bx = 0;
-	By = 0;
+	int Bx = 0;
+	int By = 0;
+
+	GFX->AppStart();
 	while (GFX->GetAppStatus() && By <= 100) {
-		ioPadGetInfo(&padinfo);
-		if (padinfo.status[0]) {
-			ioPadGetData(0, &paddata);
-			if (paddata.BTN_START) {
-				GFX->AppExit();
-				goto end;
-			}
-		}
-		Msg.ProgressBarDialogFlip();
 		frame++;
+		if (handle_pad_event(&joypads) == 2) {
+			GFX->AppExit();
+			return 2;
+		}
+
+		Msg.ProgressBarDialogFlip();
 		if (frame % 55 == 0) {
 			Bx += 10;
 			Msg.DoubleProgressBarDialogIncreaseFirstBar(10);
@@ -190,6 +222,34 @@ s32 main(s32 argc, const char* argv[]) {
 		}
 	}
 	Msg.ProgressBarDialogAbort();
+
+	return 0;
+}
+
+s32 main(s32 argc, const char* argv[]) {
+	ioPadInit(7);
+
+	pngData* png = new pngData;
+
+	NoRSX* GFX = new NoRSX(RESOLUTION_AUTO, RESOLUTION_1280x720); //set defined screen resolution You can change it to:
+	//RESOLUTION_720x480 | RESOLUTION_720x576 | RESOLUTION_1280x720 | RESOLUTION_1920x1080
+
+	if (test_bitmap(GFX) == 2 || GFX->ExitSignalStatus()) {
+		goto end;
+	}
+
+	if (test_generic(GFX) == 2 || GFX->ExitSignalStatus()) {
+		goto end;
+	}
+
+	if (test_single_bar(GFX) == 2 || GFX->ExitSignalStatus()) {
+		goto end;
+	}
+
+	if (test_double_bar(GFX) == 2 || GFX->ExitSignalStatus()) {
+		goto end;
+	}
+
 end:
 	GFX->NoRSX_Exit();
 	ioPadEnd();
